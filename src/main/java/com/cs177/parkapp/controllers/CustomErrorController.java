08ce9ad3a3319bb6cbfc490d81b3440196bbe192
Daniel Blum <drumblum@gmail.com>
2019-12-06 10:48:15 -0800

    adding status code error page and logic

---
+++ src/main/java/com/cs177/parkapp/controllers/CustomErrorController.java
@@ -0,0 +1,41 @@
+package com.cs177.parkapp.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.stream.Stream;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+  @RequestMapping({"/error"})
+  public String handleError(
+      HttpServletRequest req,
+      HttpServletResponse res,
+      Model model
+  ){
+
+    HttpStatus status =
+        Stream.of(HttpStatus.values())
+            .filter(s -> s.value() == res.getStatus())
+            .findFirst()
+            .orElse(HttpStatus.NOT_FOUND);
+
+    model.addAttribute("errorCode", status.value());
+    model.addAttribute("errorMessage", status.getReasonPhrase());
+    return "error/error";
+  }
+
+
+  @Override
+  public String getErrorPath() {
+    return "/error";
+  }
+}
