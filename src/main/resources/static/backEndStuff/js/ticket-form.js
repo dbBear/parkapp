01606392f2dccfeb5369690ebaf7934f55e48ea4
Daniel Blum <drumblum@gmail.com>
2019-12-13 12:03:37 -0800

    cleaning up unused code

--- src/main/resources/static/backEndStuff/js/ticket-form.js
+++ src/main/resources/static/backEndStuff/js/ticket-form.js
@@ -8,10 +8,7 @@ let gpsDiv = $('#gpsDiv');
 function getLocation() {
   if (navigator.geolocation) {
     gpsDiv.removeAttr('hidden');
-    // latitudeDiv.removeAttr('hidden');
-    // longitudeDiv.removeAttr('hidden');
     navigator.geolocation.getCurrentPosition(showPosition);
-
   } else {
     gpsDiv.html(
         "<p>Geolocation is not supported/allowed.</p>"
@@ -22,19 +19,6 @@ function getLocation() {
 function showPosition(position) {
   latitude.val(position.coords.latitude);
   longitude.val(position.coords.longitude);
-  // gpsDiv.append(
-  //     "<label for='latitude'>Latitude</label>" +
-  //     "<input id='latitude' type='text' class='form-control' value='" + position.coords.latitude +
-  //         "'th:field='*{latitude}'>" +
-  //     "<label for='Longitude'>Longitude</label>" +
-  //     "<input id='Longitude' type='text' class='form-control' value='" + position.coords.longitude +
-  //         "'th:field='*{longitude}'>"
-  // )
 }
 
-$(document).ready(function() {
-  // console.log("ready!")
-  // latitudeDiv.hide();
-  // longitudeDiv.hide();
-});
 /*]]>*/
